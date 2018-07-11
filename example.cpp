4dafeb92787f299defb4a13214028a530b8f2686
Kevin Server <kevinserver@yahoo.com>
2018-07-11 14:56:15 -0700

    wrote empty functions
    made parameter names more decriptive

---
+++ example.cpp
@@ -0,0 +1,17 @@
+#include "example.h"
+
+void foo()
+{
+	for (int i = 0; i < 100; i++)
+	{
+		//TODO: iterate over the thing
+	}
+}
+
+int bar(char the_character) 
+//the_character is the character to be modified
+{
+	char b = the_character+1; //modifying the character
+	//TODO: do more stuff to b
+	return 0;
+}
