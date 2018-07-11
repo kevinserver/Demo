c2afd74ab1580113e8123aa6133ef94a71d4e750
Kevin Server <kevinserver@yahoo.com>
2018-07-11 14:56:45 -0700

    wrote todos

--- example.cpp
+++ example.cpp
@@ -2,10 +2,15 @@
 
 void foo()
 {
-
+	for (int i = 0; i < 100; i++)
+	{
+		//TODO: iterate over the thing
+	}
 }
 
-int bar(char a)
+int bar(char a) //a is the character to be modified
 {
+	char b = a+1; //modifying the character
+	//TODO: do more stuff to b
 	return 0;
 }
