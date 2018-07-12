8b2d0e0aefa490aed494b151fe174cd464ccb5b4
Kevin Server <kevinserver@yahoo.com>
2018-07-12 11:45:29 -0700

    Added stuff to example.cpp
    add some more defined skeleton and TODOs

--- example.cpp
+++ example.cpp
@@ -2,10 +2,15 @@
 
 void foo()
 {
-
+	for(int i = 0; i < 100; i++)
+	{
+		//TODO: iterate over the thing
+	}
 }
 
 int bar(char mutatee)
 {
+	char b = mutatee + 1;
+	//TODO: more things to b
 	return 0;
 }
